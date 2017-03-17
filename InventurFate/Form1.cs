e6875fb041002b767cde2ff801ca31e06ef6a01b
Jersey27 <tristan.lefevre@viacesi.fr>
2017-03-17 08:42:45 +0100

    Adieu github

--- InventurFate/Form1.cs
+++ InventurFate/Form1.cs
@@ -16,5 +16,40 @@ namespace InventurFate
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
